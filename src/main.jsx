eb8838e2b3be8e07bfcc5040104b40dcb192cb4e
Christopher <odeniyichristopher7@gmail.com>
2024-08-07 16:54:42 +0100

    added redux-persist to the code base

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 //import { UserProvider } from "./components/context/UserContext.jsx";
 //import { CategoriesProvider } from "./components/context/CategoriesContext.jsx";
 //import { CartProvider } from "./components/context/CartContext.jsx";
@@ -12,9 +13,11 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,21 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore, persistReducer } from "redux-persist";
 import { rootReducers } from "./root-reducer";
+import storage from "redux-persist/lib/storage";
 
-const middleWares = [logger];
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+// eslint-disable-next-line no-undef
+const middleWares = [process.env.NODE_ENV != 'production' && logger].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
-export const store = createStore(rootReducers, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+
+export const persistor = persistStore(store);
